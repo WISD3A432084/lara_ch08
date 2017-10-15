c7c74ec3c812b9836f9eff9bbe55950422b24df9
Xin <lovemarks19@gmail.com>
2017-10-15 23:04:30 +0800

    練習四_將參數做格式限制

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,15 @@ Route::get('student/{student_no}/score',function ($student_no){
 });*/
 
 //練習三: 提供學生查詢所有科目或特定科目的成績
+/*Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+});*/
+
+//練習四: 正規表達式限制參數_將參數做格式限制
+Route::get('student/{student_no}',function ($student_no){
+    return '學號：'.$student_no;
+}) -> where(['student_no' => 's[0-9]{10}']);
+
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
-});
+}) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
