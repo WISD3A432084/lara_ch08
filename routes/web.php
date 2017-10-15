422d1e0476ae6960e2c94fee3d46f3a7869e651a
Xin <lovemarks19@gmail.com>
2017-10-15 23:27:11 +0800

    練習六_路由群組_利用prefix前綴(student)

--- routes/web.php
+++ routes/web.php
@@ -45,9 +45,12 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
 
 //練習五: 用Route的param方法替常用的參數統一限制
 Route::pattern('student_no','s[0-9]{10}');
-Route::get('student/{student_no}',function ($student_no){
-    return '學號：'.$student_no;
+//練習六: 路由群組_透過prefix前綴，將網址前套上student
+Route::group(['prefix' => 'student'],function(){
+    Route::get('{student_no}', function ($student_no) {
+        return '學號：' . $student_no;
+    });
+    Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
+        return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
+    })->where(['subject' => '(chinese|english|math)']);
 });
-Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
-    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
-}) -> where(['subject' => '(chinese|english|math)']);
