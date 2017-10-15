a0ce1c04ce3e52c4371b40b55b94bc3c87103129
Xin <lovemarks19@gmail.com>
2017-10-15 22:44:51 +0800

    練習三_定義選擇性路由參數

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,20 @@ Route::get('/', function () {
 });
 
 //練習一: 顯示學生的資料與成績
-Route::get('student/{student_no}',function ($student_no){
+/*Route::get('student/{student_no}',function ($student_no){
     return '學號：'.$student_no;
 });
 
 Route::get('student/{student_no}/score',function ($student_no){
     return '學號：'.$student_no.'的所有成績';
-});
+});*/
 
 //練習二: 提供學生查詢自己的成績
-Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
+/*Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
     return '學號：'.$student_no.'的'.$subject.'成績';
+});*/
+
+//練習三: 提供學生查詢所有科目或特定科目的成績
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
