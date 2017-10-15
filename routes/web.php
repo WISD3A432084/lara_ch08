639b7d28eddf633f3b0cbb95f9517a56f77f11ba
Xin <lovemarks19@gmail.com>
2017-10-15 23:34:54 +0800

    練習七_路由命名

--- routes/web.php
+++ routes/web.php
@@ -47,10 +47,12 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
 Route::pattern('student_no','s[0-9]{10}');
 //練習六: 路由群組_透過prefix前綴，將網址前套上student
 Route::group(['prefix' => 'student'],function(){
-    Route::get('{student_no}', function ($student_no) {
+    //練習七: 路由命名
+    Route::get('{student_no}',['as' => 'student', 'uses' => function ($student_no) {
         return '學號：' . $student_no;
-    });
-    Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
+    }]);
+    Route::get('{student_no}/score/{subject?}',['as' => 'student.score',
+        'uses' => function ($student_no, $subject = null) {
         return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
-    })->where(['subject' => '(chinese|english|math)']);
+    }])->where(['subject' => '(chinese|english|math)']);
 });
