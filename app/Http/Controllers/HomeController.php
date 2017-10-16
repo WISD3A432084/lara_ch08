84f2bf27ccc1933e9337f1ba58b1e5b52009b321
Xin <lovemarks19@gmail.com>
2017-10-16 22:23:56 +0800

    練習八_在HomeController內加入indexc函數

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //練習八: 在HomeController內加入indexc函數
+    public function indexc(){
+        return view('welcome');
+    }
 }
