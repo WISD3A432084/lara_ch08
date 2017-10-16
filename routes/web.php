1dc3c6388edd59cc172d88228dcd6f955ea34471
Xin <lovemarks19@gmail.com>
2017-10-16 23:44:38 +0800

    練習十_修改路由'cool'，使之加入namespace路由'Cool'當中

--- routes/web.php
+++ routes/web.php
@@ -54,4 +54,7 @@ Route::group(['prefix' => 'student'],function(){
 });
 
 //練習十: 新增路由'cool'
-Route::get('cool', 'Cool\TestController@indexc');
+//練習十: 修改路由'cool'，使之加入namespace路由'Cool'當中
+Route::group(['namespace' => 'Cool'],function (){
+    Route::get('cool', 'TestController@indexc');
+});
