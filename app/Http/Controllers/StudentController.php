e11eb32d720df610cfa13de866f13113205e0894
Xin <lovemarks19@gmail.com>
2017-10-16 22:41:28 +0800

    練習九_加入getStudentData及getStudentScore函數

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    //練習九: 加入getStudentData及getStudentScore函數
+    public function getStudentData($student_no){
+        return "學號: ".$student_no;
+    }
+
+    public function getStudentScore($student_no, $subject = null){
+        return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+    }
 }
