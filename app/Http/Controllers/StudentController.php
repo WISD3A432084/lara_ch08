edda5e39a3236140a889decaa6a90a3f1bc835d7
Xin <lovemarks19@gmail.com>
2017-10-25 17:07:30 +0800

    修改 StudentController 的兩個方法，使之可傳入 student_no 或 subjec t的資料

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Student;
+use View;
 
 class StudentController extends Controller
 {
     //練習九: 加入getStudentData及getStudentScore函數
     public function getStudentData($student_no){
-        return "學號: ".$student_no;
+        //return "學號: ".$student_no;
+        return View::make('score',['student_no' => $student_no,
+            'subject' => null]);
     }
 
     public function getStudentScore($student_no, $subject = null){
-        return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+        //return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+        return View::make('score', ['student_no' => $student_no,
+            'subject' => $subject]);
     }
 }
