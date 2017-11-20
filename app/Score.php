ebc08d6f1a599159b163d9ba39007bd89765fbdb
Xin <lovemarks19@gmail.com>
2017-11-21 00:13:24 +0800

    建立兩個scope方法：scopeOrderByTotal() 及 scopeOrderBySubject()，用以對查詢的資料作排序

--- app/Score.php
+++ app/Score.php
@@ -12,4 +12,14 @@ class Score extends Model
     public function student(){
         return $this->belongsTo(Student::class);
     }
+
+    public function scopeOrderByTotal($query){
+        return $query->orderBy('score.total','DESC');
+    }
+
+    public function scopeOrderBySubject($query){
+        return $query->orderBy('score.chinese','DESC')
+                    ->orderBy('score.english','DESC')
+                    ->orderBy('score.math','DESC');
+    }
 }
