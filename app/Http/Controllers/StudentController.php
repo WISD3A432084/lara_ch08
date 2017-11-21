0d4133b767acc2ea1ce589974003fab7ccaeecb3
Xin <lovemarks19@gmail.com>
2017-11-21 13:41:47 +0800

    練習五_修改 StudentController 之 getStudentData()方法及 getStudentScore() 方法，使之由DB 取得某一學生的資料($student)，包括其帳號資料($student->user)、國英數成績($student->score)，傳遞給student 視圖呈現

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,15 +10,30 @@ use View;
 class StudentController extends Controller
 {
     //練習九: 加入getStudentData及getStudentScore函數
+    //Ch08_練習五
     public function getStudentData($student_no){
         //return "學號: ".$student_no;
-        return View::make('score',['student_no' => $student_no,
-            'subject' => null]);
+        /*return View::make('score',['student_no' => $student_no,
+            'subject' => null]);*/
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
+            'subject'=>null
+        ]);
     }
 
     public function getStudentScore($student_no, $subject = null){
         //return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
-        return View::make('score', ['student_no' => $student_no,
-            'subject' => $subject]);
+        /*return View::make('score', ['student_no' => $student_no,
+            'subject' => $subject]);*/
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
+            'subject'=>$subject
+        ]);
     }
 }
