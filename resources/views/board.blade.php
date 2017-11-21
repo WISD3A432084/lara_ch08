f4be8376615378f14996aae17be5fd1bdaf57622
Xin <lovemarks19@gmail.com>
2017-11-21 22:07:52 +0800

    修改board 視圖上的按鈕，並測試按鈕可以顯示該學生的資料與成績

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -46,7 +46,7 @@
 							<div>{{ $score->total }}</div>
 						</td>
 						<td>
-							<a class="btn btn-default btn-sm" href="{{route('student',['student_no'=>'s1234567890'])}}">查看學生資料</a>
+							<a class="btn btn-default btn-sm" href="{{route('student',['student_no'=> $score->student->no])}}">查看學生資料</a>
 						</td>
 						<td>
 					</tr>
